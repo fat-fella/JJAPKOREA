977eec9b7b675e8e408171e95859dd948ec3926f
fat-fella <aznclee711@gmail.com>
2023-07-31 20:24:53 +0900

    회원가입 도전

--- jjapKorea_kh_semi/src/main/java/jjapkorea/member/controller/SignServlet.java
+++ jjapKorea_kh_semi/src/main/java/jjapkorea/member/controller/SignServlet.java
@@ -26,6 +26,7 @@ public class SignServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/member/signup.jsp").forward(request, response);
+		
 	}
 
 	/**
