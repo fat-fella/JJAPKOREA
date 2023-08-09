d7f9158f794da160969d68d6428c03a019a03ff4
yurim0227 <byurim227@gmail.com>
2023-08-09 12:24:35 +0900

    .

---
+++ jjapKorea_kh_semi/src/main/java/jjapkorea/member/controller/LogoutServlet.java
@@ -0,0 +1,27 @@
+package jjapkorea.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().removeAttribute("SsLoginId");
+		request.getSession().setAttribute("successFailMsg", "로그아웃되었습니다.");
+		response.sendRedirect(request.getContextPath()+"/index");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
