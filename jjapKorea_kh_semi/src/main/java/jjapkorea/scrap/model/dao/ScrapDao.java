bc9506dea1d881b7d881364df305b06e59d26e6f
fat-fella <aznclee711@gmail.com>
2023-08-09 17:49:14 +0900

    스크랩 기능한건데 이게 맞는지 모르겠다 ㅋ_ㅋ

---
+++ jjapKorea_kh_semi/src/main/java/jjapkorea/scrap/model/dao/ScrapDao.java
@@ -0,0 +1,53 @@
+package jjapkorea.scrap.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static jjapkorea.common.jdbc.JdbcTemplate.*;
+import jjapkorea.scrap.model.dto.ScrapDto;
+
+public class ScrapDao {
+	public List<ScrapDto> scrapList(Connection conn){
+		List<ScrapDto> result = null;
+		String query = "select * from SCRAP";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			pstmt= conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			result = new ArrayList<ScrapDto>();	
+			while(rs.next()==true) {
+				ScrapDto dto = new ScrapDto(
+				dto.setJid(rs.getString("jid")),
+				dto.setMid(rs.getString("mid")),
+				dto.setRecruitField(rs.getString("recruit_field")),
+				dto.setCompanyName(rs.getString("company_name")),
+				dto.setBusiNo(rs.getString("busino")),
+				dto.setUserEducation(rs.getString("user_education")),
+				dto.setSalary(rs.getString("salary")),
+				dto.setMinSalary(rs.getString("min_salary")),
+				dto.setMaxSalary(rs.getString("max_salary")),
+				dto.setRegistDate(rs.getString("regist_date")),
+				dto.setCloseDate(rs.getString("close_date")),
+				dto.setReTitle(rs.getString("re_title")),
+				dto.setCareer(rs.getString("career")),
+				dto.setWorkType(rs.getString("work_type")),
+				dto.setEmpTypeCode(rs.getString("emp_type_code"))
+						);
+				result.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			rs.close();
+			conn.close();
+		}
+		
+		
+		
+		return result;
+	};
+}
---
+++ jjapKorea_kh_semi/src/main/java/jjapkorea/scrap/model/dto/ScrapDto.java
@@ -0,0 +1,203 @@
+package jjapkorea.scrap.model.dto;
+
+public class ScrapDto {
+//	"JID"	NUMBER		NOT NULL,
+//	"MID"	VARCHAR2(20)		NOT NULL,
+//	"RECRUIT_FIELD"	VARCHAR2(20)		NULL,
+//	"COMPANY_NAME"	VARCHAR2(255)		NULL,
+//	"BUSINO"	VARCHAR(20)		NULL,
+//	"USER_EDUCATION"	VARCHAR(50)		NULL,
+//	"SALARY"	VARCHAR(50)		NULL,
+//	"MIN_SALARY"	VARCHAR(50)		NULL,
+//	"MAX_SALARY"	VARCHAR(50)		NULL,
+//	"REGIST_DATE"	VARCHAR(20)		NULL,
+//	"CLOSE_DATE"	VARCHAR(20)		NULL,
+//	"RE_TITLE"	VARCHAR(255)		NULL,
+//	"CAREER"	VARCHAR(50)		NULL,
+//	"WORK_TYPE"	VARCHAR(50)		NULL,
+//	"EMP_TYPE_CODE"	VARCHAR(20)		NULL
+
+	
+	private int jid;
+	private String mid;
+	private String recruitField;
+	private String companyName;
+	private String busiNo;
+	private String userEducation;
+	private String salary;
+	private String minSalary;
+	private String maxSalary;
+	private String registDate;
+	private String closeDate;
+	private String reTitle;
+	private String career;
+	private String workType;
+	private String empTypeCode;
+	
+	@Override
+	public String toString() {
+		return "ScrapDto [jid=" + jid + ", mid=" + mid + ", recruitField=" + recruitField + ", companyName="
+				+ companyName + ", busiNo=" + busiNo + ", userEducation=" + userEducation + ", salary=" + salary
+				+ ", minSalary=" + minSalary + ", maxSalary=" + maxSalary + ", registDate=" + registDate
+				+ ", closeDate=" + closeDate + ", reTitle=" + reTitle + ", career=" + career + ", workType=" + workType
+				+ ", empTypeCode=" + empTypeCode + "]";
+	}
+
+	
+	
+	
+	
+	public ScrapDto() {
+		super();
+	}
+
+	
+
+
+
+	public ScrapDto(int jid, String mid, String recruitField, String companyName, String busiNo, String userEducation,
+			String salary, String minSalary, String maxSalary, String registDate, String closeDate, String reTitle,
+			String career, String workType, String empTypeCode) {
+		super();
+		this.jid = jid;
+		this.mid = mid;
+		this.recruitField = recruitField;
+		this.companyName = companyName;
+		this.busiNo = busiNo;
+		this.userEducation = userEducation;
+		this.salary = salary;
+		this.minSalary = minSalary;
+		this.maxSalary = maxSalary;
+		this.registDate = registDate;
+		this.closeDate = closeDate;
+		this.reTitle = reTitle;
+		this.career = career;
+		this.workType = workType;
+		this.empTypeCode = empTypeCode;
+	}
+
+
+
+
+
+	public int getJid() {
+		return jid;
+	}
+
+	public void setJid(int jid) {
+		this.jid = jid;
+	}
+
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	public String getRecruitField() {
+		return recruitField;
+	}
+
+	public void setRecruitField(String recruitField) {
+		this.recruitField = recruitField;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getBusiNo() {
+		return busiNo;
+	}
+
+	public void setBusiNo(String busiNo) {
+		this.busiNo = busiNo;
+	}
+
+	public String getUserEducation() {
+		return userEducation;
+	}
+
+	public void setUserEducation(String userEducation) {
+		this.userEducation = userEducation;
+	}
+
+	public String getSalary() {
+		return salary;
+	}
+
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+
+	public String getMinSalary() {
+		return minSalary;
+	}
+
+	public void setMinSalary(String minSalary) {
+		this.minSalary = minSalary;
+	}
+
+	public String getMaxSalary() {
+		return maxSalary;
+	}
+
+	public void setMaxSalary(String maxSalary) {
+		this.maxSalary = maxSalary;
+	}
+
+	public String getRegistDate() {
+		return registDate;
+	}
+
+	public void setRegistDate(String registDate) {
+		this.registDate = registDate;
+	}
+
+	public String getCloseDate() {
+		return closeDate;
+	}
+
+	public void setCloseDate(String closeDate) {
+		this.closeDate = closeDate;
+	}
+
+	public String getReTitle() {
+		return reTitle;
+	}
+
+	public void setReTitle(String reTitle) {
+		this.reTitle = reTitle;
+	}
+
+	public String getCareer() {
+		return career;
+	}
+
+	public void setCareer(String career) {
+		this.career = career;
+	}
+
+	public String getWorkType() {
+		return workType;
+	}
+
+	public void setWorkType(String workType) {
+		this.workType = workType;
+	}
+
+	public String getEmpTypeCode() {
+		return empTypeCode;
+	}
+
+	public void setEmpTypeCode(String empTypeCode) {
+		this.empTypeCode = empTypeCode;
+	}
+	
+}
