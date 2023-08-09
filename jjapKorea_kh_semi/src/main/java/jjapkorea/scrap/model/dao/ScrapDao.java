02e0c6fbed8977a4f61109da773a351db20ff6e9
fat-fella <aznclee711@gmail.com>
2023-08-09 18:49:42 +0900

    scrap dao, scrap service

--- jjapKorea_kh_semi/src/main/java/jjapkorea/scrap/model/dao/ScrapDao.java
+++ jjapKorea_kh_semi/src/main/java/jjapkorea/scrap/model/dao/ScrapDao.java
@@ -20,30 +20,29 @@ public class ScrapDao {
 			rs = pstmt.executeQuery();
 			result = new ArrayList<ScrapDto>();	
 			while(rs.next()==true) {
-				ScrapDto dto = new ScrapDto(
-				dto.setJid(rs.getString("jid")),
-				dto.setMid(rs.getString("mid")),
-				dto.setRecruitField(rs.getString("recruit_field")),
-				dto.setCompanyName(rs.getString("company_name")),
-				dto.setBusiNo(rs.getString("busino")),
-				dto.setUserEducation(rs.getString("user_education")),
-				dto.setSalary(rs.getString("salary")),
-				dto.setMinSalary(rs.getString("min_salary")),
-				dto.setMaxSalary(rs.getString("max_salary")),
-				dto.setRegistDate(rs.getString("regist_date")),
-				dto.setCloseDate(rs.getString("close_date")),
-				dto.setReTitle(rs.getString("re_title")),
-				dto.setCareer(rs.getString("career")),
-				dto.setWorkType(rs.getString("work_type")),
-				dto.setEmpTypeCode(rs.getString("emp_type_code"))
-						);
+				ScrapDto dto = new ScrapDto();
+				dto.setJid(rs.getInt("jid"));
+				dto.setMid(rs.getString("mid"));
+				dto.setRecruitField(rs.getString("recruit_field"));
+				dto.setCompanyName(rs.getString("company_name"));
+				dto.setBusiNo(rs.getString("busino"));
+				dto.setUserEducation(rs.getString("user_education"));
+				dto.setSalary(rs.getString("salary"));
+				dto.setMinSalary(rs.getString("min_salary"));
+				dto.setMaxSalary(rs.getString("max_salary"));
+				dto.setRegistDate(rs.getString("regist_date"));
+				dto.setCloseDate(rs.getString("close_date"));
+				dto.setReTitle(rs.getString("re_title"));
+				dto.setCareer(rs.getString("career"));
+				dto.setWorkType(rs.getString("work_type"));
+				dto.setEmpTypeCode(rs.getString("emp_type_code"));
 				result.add(dto);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			rs.close();
-			conn.close();
+			close(rs);
+			close(pstmt);
 		}
 		
 		
