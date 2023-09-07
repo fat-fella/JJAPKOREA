8f6e02b100a711abc179ebb10442fc979f6e64cd
jiiin070496 <jin03154@naver.com>
2023-09-07 17:28:55 +0900

    취업
    톡톡

---
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/board/model/dao/BoardDao.java
@@ -0,0 +1,35 @@
+package kh.lclass.jjapkorea.board.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kh.lclass.jjapkorea.board.model.dto.BoardDto;
+
+@Repository
+public class BoardDao {
+	@Autowired
+	private SqlSession sqlSession; //root-context.xml에 bean SqlSession객체에 적혀있는 아이디
+	
+	public List<BoardDto> selectList() throws Exception{ 
+		return sqlSession.selectList("board.selectList");
+	}
+	public BoardDto selectOne(int bno) throws Exception{
+		return sqlSession.selectOne("board.selectOne", bno);
+	}
+	// insert 시 selectKey를 이용해 PK 값이 추가되어있는 자료형
+	public BoardDto insert(BoardDto dto) throws Exception{
+		int result = sqlSession.insert("board.insert", dto);
+		return dto;
+	}
+	//답글
+
+	public int update(BoardDto dto) throws Exception{
+		return sqlSession.update("board.update", dto);
+	}
+	public int delete(int bno) throws Exception{
+		return sqlSession.delete("board.delete", bno);
+	}
+}
---
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/board/model/service/BoardService.java
@@ -0,0 +1,36 @@
+package kh.lclass.jjapkorea.board.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kh.lclass.jjapkorea.board.model.dao.BoardDao;
+import kh.lclass.jjapkorea.board.model.dto.BoardDto;
+
+@Service
+public class BoardService {
+	@Autowired
+	private BoardDao boardDao; 
+	
+	public List<BoardDto> selectList() throws Exception{
+		return boardDao.selectList();
+	}
+	public BoardDto selectOne(int bno) throws Exception{
+		return boardDao.selectOne(bno);
+	}
+	@Transactional // 바로 밑에있는 메소드 하나에만 영향을 줌 
+	public int insert(BoardDto dto) throws Exception{
+		BoardDto returnVo = boardDao.insert(dto);
+		int result = returnVo.getBno();
+		return result;
+	}
+
+	public int update(BoardDto dto) throws Exception{
+		return boardDao.update(dto);
+	}
+	public int delete(int bno) throws Exception{
+		return boardDao.delete(bno);
+	}
+}
