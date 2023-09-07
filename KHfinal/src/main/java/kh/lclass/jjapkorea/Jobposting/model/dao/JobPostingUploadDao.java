77070fea443ffbb6a1476615e9d1565c169ed712
fat-fella <aznclee711@gmail.com>
2023-09-07 20:22:07 +0900

    복구작업

---
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/Jobposting/model/dao/JobPostingUploadDao.java
@@ -0,0 +1,21 @@
+package kh.lclass.jjapkorea.Jobposting.model.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kh.lclass.jjapkorea.Jobposting.model.dto.JobpostingDto;
+
+@Repository("jobPostinguploadDao")
+public class JobPostingUploadDao {
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int insert(JobpostingDto dto) {
+		return sqlSession.insert("jobPostingUpload.insert", dto);
+	}
+	
+	
+	
+	
+}
