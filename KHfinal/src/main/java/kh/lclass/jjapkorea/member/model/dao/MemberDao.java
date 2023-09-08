f845faee8ce68a93670198a634659861c872f1d3
yurim0227 <byurim227@gmail.com>
2023-09-08 16:19:42 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/member/model/dao/MemberDao.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/member/model/dao/MemberDao.java
@@ -39,24 +39,6 @@ public class MemberDao {
 	}
 
 	public void insert(SqlSession session) {
-		/*
-		 * public void insert(Connection conn) { String checkQuery =
-		 * "SELECT COUNT(*) FROM businessform"; String insertQuery =
-		 * "INSERT INTO businessform VALUES (?, ?)"; List<String> list = new
-		 * ArrayList<String>(); list.add("대기업");        list.add("대기업 계열사·자회사");
-		 * list.add("중소기업(300명이하)"); list.add("중견기업(300명이상)"); list.add("벤처기업");
-		 * list.add("외국계(외국 투자기업)"); list.add("외국계(외국 법인기업)"); list.add("국내 공공기관·공기업");
-		 * list.add("비영리단체·협회·교육재단"); list.add("외국 기관·비영리기구·단체"); PreparedStatement
-		 * checkStmt = null; PreparedStatement insertStmt = null; try { checkStmt =
-		 * conn.prepareStatement(checkQuery); ResultSet rs = checkStmt.executeQuery();
-		 * if (rs.next() && rs.getInt(1) == 0) { insertStmt =
-		 * conn.prepareStatement(insertQuery); for (int i = 0; i < list.size(); i++) {
-		 * insertStmt.setInt(1, i + 1); insertStmt.setString(2, list.get(i))
-		 * insertStmt.executeUpdate(); } } for (int i = 0; i < list.size(); i++) {
-		 * System.out.println(list.get(i)); } } catch (Exception e) {
-		 * e.printStackTrace(); } finally { close(checkStmt); close(insertStmt); } }
-		 */
-		//TODO
 		String namespace = "member";
         String checkQuery = namespace + ".check";
 
