9473e8de021237121f0b6807dc4f28e3807a1f85
fat-fella <aznclee711@gmail.com>
2023-09-13 20:54:58 +0900

    채팅 db작업 수정 중

--- KHfinal/src/main/java/kh/lclass/jjapkorea/chat/model/repository/ChatRoomRepository.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/chat/model/repository/ChatRoomRepository.java
@@ -11,29 +11,37 @@ import java.util.stream.Stream;
 @Repository
 public class ChatRoomRepository {
 
-    private Map<String, ChatRoomDto> chatRoomDtoMap;
+    //private Map<String, ChatRoomDto> chatRoomDtoMap;
+	//session ~~~~
+	
+	
+//    @PostConstruct
+//    private void init(){
+//        chatRoomDtoMap = new LinkedHashMap<>();
+//    }
 
-    @PostConstruct
-    private void init(){
-        chatRoomDtoMap = new LinkedHashMap<>();
-    }
-
+	
+	
+	
+	
     public List<ChatRoomDto> findAllRooms(){
         //채팅방 생성 순서 최근 순으로 반환
-        List<ChatRoomDto> result = new ArrayList<>(chatRoomDtoMap.values());
-        Collections.reverse(result);
-
-        return result;
+//        List<ChatRoomDto> result = new ArrayList<>(chatRoomDtoMap.values()); //맵퍼 session.selectlist
+//        Collections.reverse(result);
+//        return result;
+    	return null;
     }
 
     public ChatRoomDto findRoomById(String id){
-        return chatRoomDtoMap.get(id);
+//        return chatRoomDtoMap.get(id); //session.selectone
+    	return null;
     }
 
     public ChatRoomDto createChatRoomDto(String name){
         ChatRoomDto room = ChatRoomDto.create(name);
-        chatRoomDtoMap.put(room.getRoomId(), room);
+//        chatRoomDtoMap.put(room.getRoomId(), room);  //session.insert 
 
         return room;
     }
+    //chat content db
 }
