0c0a628baea62221f322f623d1b17de4b8469796
yurim0227 <byurim227@gmail.com>
2023-09-14 17:46:21 +0900

    주석 처리

--- KHfinal/src/main/java/kh/lclass/jjapkorea/swp/auth/SnsValue.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/swp/auth/SnsValue.java
@@ -1,45 +1,45 @@
-package kh.lclass.jjapkorea.swp.auth;
-
-import org.apache.commons.lang3.StringUtils;
-
-import com.github.scribejava.apis.GoogleApi20;
-import com.github.scribejava.core.builder.api.DefaultApi20;
-
-import lombok.Data;
-
-@Data
-public class SnsValue implements SnsUrls {
-	private String service;
-	private String clientId;
-	private String clientSecret;
-	private String redirectUrl;
-	private DefaultApi20 api20Instance;
-	private String profileUrl;
-	
-	private boolean isNaver;
-	private boolean isGoogle;
-	private boolean isKakao;
-	
-	public SnsValue(String service, String cid, String cs, String rurl) {
-		this.service = service;
-		this.clientId = cid;
-		this.clientSecret = cs;
-		this.redirectUrl = rurl;
-		
-		this.isNaver = StringUtils.equalsIgnoreCase("naver", this.service);
-		this.isGoogle = StringUtils.equalsIgnoreCase("google", this.service);
-		this.isKakao = StringUtils.equalsIgnoreCase("kakao", this.service);
-		
-		if (isNaver) {
-			this.api20Instance = NaverAPI20.instance();
-			this.profileUrl = NAVER_PROFILE_URL;
-			
-		} else if (isGoogle) {
-			this.api20Instance = GoogleApi20.instance();
-			this.profileUrl = GOOGLE_PROFILE_URL;
-//		} else if (isKakao) {
-//			this.api20Instance = KakaoApi20.instance();
+//package kh.lclass.jjapkorea.swp.auth;
+//
+//import org.apache.commons.lang3.StringUtils;
+//
+//import com.github.scribejava.apis.GoogleApi20;
+//import com.github.scribejava.core.builder.api.DefaultApi20;
+//
+//import lombok.Data;
+//
+//@Data
+//public class SnsValue implements SnsUrls {
+//	private String service;
+//	private String clientId;
+//	private String clientSecret;
+//	private String redirectUrl;
+//	private DefaultApi20 api20Instance;
+//	private String profileUrl;
+//	
+//	private boolean isNaver;
+//	private boolean isGoogle;
+//	private boolean isKakao;
+//	
+//	public SnsValue(String service, String cid, String cs, String rurl) {
+//		this.service = service;
+//		this.clientId = cid;
+//		this.clientSecret = cs;
+//		this.redirectUrl = rurl;
+//		
+//		this.isNaver = StringUtils.equalsIgnoreCase("naver", this.service);
+//		this.isGoogle = StringUtils.equalsIgnoreCase("google", this.service);
+//		this.isKakao = StringUtils.equalsIgnoreCase("kakao", this.service);
+//		
+//		if (isNaver) {
+//			this.api20Instance = NaverAPI20.instance();
+//			this.profileUrl = NAVER_PROFILE_URL;
+//			
+//		} else if (isGoogle) {
+//			this.api20Instance = GoogleApi20.instance();
 //			this.profileUrl = GOOGLE_PROFILE_URL;
-		}
-	}
-}
+////		} else if (isKakao) {
+////			this.api20Instance = KakaoApi20.instance();
+////			this.profileUrl = GOOGLE_PROFILE_URL;
+//		}
+//	}
+//}
--- KHfinal/src/main/java/kh/lclass/jjapkorea/swp/dto/LoginDTO.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/swp/dto/LoginDTO.java
@@ -1,9 +1,9 @@
-package kh.lclass.jjapkorea.swp.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDTO {
-	private String uid;
-	private String upw;
-}
+//package kh.lclass.jjapkorea.swp.dto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class LoginDTO {
+//	private String uid;
+//	private String upw;
+//}
