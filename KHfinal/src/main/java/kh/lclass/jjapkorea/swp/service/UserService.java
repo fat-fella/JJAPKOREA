4245e3b538f924cacfe5aaa169d8083ec5cbb1d9
Minji0A0 <mingmingji0a0@gmail.com>
2023-09-14 17:39:45 +0900

    Revert "Revert "API SNS Login Bagic""
    This reverts commit e349e8ea6adf026fe5cc38d79c42593c578f7160.

---
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/swp/service/UserService.java
@@ -0,0 +1,14 @@
+package kh.lclass.jjapkorea.swp.service;
+
+import java.util.Date;
+
+import kh.lclass.jjapkorea.swp.domain.User;
+import kh.lclass.jjapkorea.swp.dto.LoginDTO;
+
+public interface UserService {
+	User login(LoginDTO dto) throws Exception;
+	void keepLogin(String uid, String id, Date expire);
+	User checkLoginBefore(String value);
+	User getBySns(User snsUser);
+
+}
