e05d34b6f2719dc95de3d09d18d94bfc178f9bf5
Minji0A0 <mingmingji0a0@gmail.com>
2023-09-19 17:18:40 +0900

    API SNS LOGIN // Login controller 수정
    servlet 주석 처리 및 API Sns controller 적용

--- KHfinal/src/main/java/kh/lclass/jjapkorea/member/controller/LoginServlet.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/member/controller/LoginServlet.java
@@ -1,19 +1,19 @@
-package kh.lclass.jjapkorea.member.controller;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-@WebServlet("/login")
-public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/view/member/login.jsp").forward(request, response);
-	}
-
-}
+//package kh.lclass.jjapkorea.member.controller;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//@WebServlet("/login")
+//public class LoginServlet extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		request.getRequestDispatcher("/WEB-INF/view/member/login.jsp").forward(request, response);
+//	}
+//
+//}
