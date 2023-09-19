ee8d6813ef9c9f791ffc6ba7f76d25a994839606
jiiin070496 <jin03154@naver.com>
2023-09-19 11:47:37 +0900

    boardDto lombok

--- KHfinal/src/main/java/kh/lclass/jjapkorea/board/model/dto/BoardDto.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/board/model/dto/BoardDto.java
@@ -2,9 +2,13 @@ package kh.lclass.jjapkorea.board.model.dto;
 
 import org.springframework.stereotype.Component;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @Component
 public class BoardDto {
 
@@ -16,5 +20,11 @@ public class BoardDto {
 	private int bref;
 	private int breLevel;
 	private int breStep;
+	@Override
+	public String toString() {
+		return "BoardDto [bno=" + bno + ", btitle=" + btitle + ", bwriteDate=" + bwriteDate
+				+ ", mid=" + mid + ", bref=" + bref + ", breLevel=" + breLevel + ", breStep=" + breStep + "]";
+	}
 
+	
 }
