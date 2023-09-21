d8f4a4513dde468950773ed5b0a97ec077fdf4e6
Minji0A0 <mingmingji0a0@gmail.com>
2023-09-21 23:12:14 +0900

    member 개인가입자 servlet 주석처리 // API SNS LOGIN controller 변경
    -APISnsPsignupController로 대체

--- KHfinal/src/main/java/kh/lclass/jjapkorea/member/controller/PSignServlet.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/member/controller/PSignServlet.java
@@ -1,18 +1,18 @@
-package kh.lclass.jjapkorea.member.controller;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/psignup")
-public class PSignServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	request.getRequestDispatcher("WEB-INF/view/member/psignup.jsp").forward(request, response);
-	}
-
-}
+//package kh.lclass.jjapkorea.member.controller;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//@WebServlet("/psignup")
+//public class PSignServlet extends HttpServlet {
+//	private static final long serialVersionUID = 1L;
+//
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	request.getRequestDispatcher("WEB-INF/view/member/psignup.jsp").forward(request, response);
+//	}
+//
+//}
