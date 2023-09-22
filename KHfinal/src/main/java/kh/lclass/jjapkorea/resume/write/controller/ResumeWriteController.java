275c0b9ccd9b313b05a212d5abc9af8f2a9787b4
Solbeen0531 <netps0201@gmail.com>
2023-09-22 15:20:50 +0900

    Update ResumeWriteController.java
    서블릿을 컨트롤러로 통합

--- KHfinal/src/main/java/kh/lclass/jjapkorea/resume/write/controller/ResumeWriteController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/resume/write/controller/ResumeWriteController.java
@@ -1,5 +1,15 @@
 package kh.lclass.jjapkorea.resume.write.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ResumeWriteController {
 
+	@GetMapping("resume/write")
+	public String resumeMgr() {
+
+		return "/resume/resumeWrite";
+
+	}
 }
