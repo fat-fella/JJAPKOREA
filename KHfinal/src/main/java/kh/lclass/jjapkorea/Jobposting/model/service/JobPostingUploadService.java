53940ca5816e17c8bc5cd333a45985203c4f0e9d
fat-fella <aznclee711@gmail.com>
2023-09-29 16:56:33 +0900

    Update JobPostingUploadService.java

--- KHfinal/src/main/java/kh/lclass/jjapkorea/Jobposting/model/service/JobPostingUploadService.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/Jobposting/model/service/JobPostingUploadService.java
@@ -1,10 +1,6 @@
 package kh.lclass.jjapkorea.Jobposting.model.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import kh.lclass.jjapkorea.Jobposting.model.dao.JobPostingUploadDao;
 import kh.lclass.jjapkorea.Jobposting.model.dto.JobpostingDto;
 
 
