a9e0fa727f5ecdaa50524c2318a5339b3e498edb
yurim0227 <byurim227@gmail.com>
2023-09-30 13:30:21 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/member/model/dto/BusinessFormDto.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/member/model/dto/BusinessFormDto.java
@@ -1,14 +1,13 @@
 package kh.lclass.jjapkorea.member.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BusinessFormDto {
 	private int bform;
     private String bformName;
-    
-    public BusinessFormDto(int bform, String bformName) {
-        this.bform = bform;
-        this.bformName = bformName;
-    }
 }
