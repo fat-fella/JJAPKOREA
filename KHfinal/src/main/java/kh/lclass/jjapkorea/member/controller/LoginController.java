d4f5908da1fb42660034118e17597bef005707b3
yurim0227 <byurim227@gmail.com>
2023-10-01 00:30:49 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/member/controller/LoginController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/member/controller/LoginController.java
@@ -21,40 +21,4 @@ public class LoginController {
 	public String loginAdmin() throws Exception {
 		return "member/loginAdmin";
 	}
-	
-//	@PostMapping("/admin")
-//	public ResponseEntity<String> loginAdmin(MemberDto memberDto, HttpServletRequest request, RedirectAttributes redirectAttr) throws Exception {
-//		HttpSession session = request.getSession();
-//		MemberDto loginAdmin = memberService.loginAdmin(memberDto);
-//		if(loginAdmin != null) {
-//			session.setAttribute("loginAdmin", loginAdmin);
-//			return ResponseEntity.ok("/jjapkorea/index");
-//		} else {
-//			return ResponseEntity.badRequest().body("로그인 실패");
-//		}
-//	}
-//	
-//	@PostMapping("/person")
-//	public ResponseEntity<String> loginPerson(MemberDto memberDto, HttpServletRequest request, RedirectAttributes redirectAttr) throws Exception {
-//		HttpSession session = request.getSession();
-//		MemberDto loginPerson = memberService.loginPerson(memberDto);
-//		if(loginPerson != null) {
-//			session.setAttribute("loginPerson", loginPerson);
-//			return ResponseEntity.ok("/jjapkorea/index");
-//		} else {
-//			return ResponseEntity.badRequest().body("로그인 실패");
-//		}
-//	}
-//	
-//	@PostMapping("/business")
-//	public ResponseEntity<String> loginBusiness(MemberDto memberDto, HttpServletRequest request) throws Exception {
-//		HttpSession session = request.getSession();
-//		MemberDto loginBusiness = memberService.loginBusiness(memberDto);
-//		if(loginBusiness != null) {
-//			session.setAttribute("loginBusiness", loginBusiness);
-//			return ResponseEntity.ok("/jjapkorea/index");
-//		} else {
-//			return ResponseEntity.badRequest().body("로그인 실패");
-//		}
-//	}
 }
