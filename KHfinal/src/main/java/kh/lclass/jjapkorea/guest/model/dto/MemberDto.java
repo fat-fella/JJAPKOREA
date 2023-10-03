b9bcc33b06077b152404ec8f0236c8ef12aedfe5
yurim0227 <byurim227@gmail.com>
2023-10-03 14:05:37 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/guest/model/dto/MemberDto.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/guest/model/dto/MemberDto.java
@@ -1,10 +1,18 @@
 package kh.lclass.jjapkorea.guest.model.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class MemberDto {
+	@NotEmpty(message = "아이디를 입력하세요")
 	private String mid;
+	@NotEmpty(message = "비밀번호를 입력하세요")
+    @Size(min = 8, max = 16, message = "비밀번호는 8자에서 16자 사이여야 합니다")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).*$", message = "비밀번호는 대문자, 소문자, 특수문자를 포함해야 합니다")
 	private String mpw;
 	private String mtype;
 	private int enabled;
--- KHfinal/src/main/java/kh/lclass/jjapkorea/guest/model/dto/PersonDto.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/guest/model/dto/PersonDto.java
@@ -1,11 +1,16 @@
 package kh.lclass.jjapkorea.guest.model.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 import lombok.Data;
 
 @Data
 public class PersonDto {
 	private String mid;
+	@NotEmpty(message = "이름을 입력하세요")
 	private String pname;
+	@NotEmpty(message = "이메일을 입력하세요")
 	private String pemail;
+	@NotEmpty(message = "전화번호를 입력하세요")
 	private String pphone;
 }
