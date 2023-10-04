105b96cadcab46a3206bc5216cf2261ce5c3546c
yurim0227 <byurim227@gmail.com>
2023-10-04 14:38:26 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/guest/controller/IndexController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/guest/controller/IndexController.java
@@ -2,9 +2,11 @@ package kh.lclass.jjapkorea.guest.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -18,28 +20,42 @@ import kh.lclass.jjapkorea.api.WorknetApi;
 @Controller
 public class IndexController {
     @Autowired
-	private MemberService memberService;
+    private MemberService memberService;
     
-	@Autowired
-	private WorknetApi worknetApi;
+    @Autowired
+    private WorknetApi worknetApi;
     
+    private List<JobPostingDto> updatedJobPostings; // 업데이트된 데이터를 저장할 변수
+
+    @PostConstruct // 스프링 빈 초기화 시 한 번 호출
+    public void init() {
+        // 초기 데이터 업데이트
+        updateJobPostings();
+    }
+
+    // 주기적으로 데이터를 업데이트하는 메소드
+    @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행 (cron 표현식)
+    public void updateJobPostings() {
+        updatedJobPostings = worknetApi.getJobPostings();
+    }
+
     @GetMapping("/index")
     public String index(Model model, HttpSession session) throws Exception {
-    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    	String mid = authentication.getName();
-    	String pname = memberService.selectOnePerson(mid);
-    	model.addAttribute("pname", pname);
-    	
-        List<JobPostingDto> list = worknetApi.getJobPostings();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String mid = authentication.getName();
+        String pname = memberService.selectOnePerson(mid);
+        model.addAttribute("pname", pname);
+        
+        // 업데이트된 데이터 사용
         int chunkSize = 8; // 묶을 요소 개수
-        int totalItems = list.size();
+        int totalItems = updatedJobPostings.size();
         for (int startIndex = 0; startIndex < totalItems; startIndex += chunkSize) {
             int endIndex = Math.min(startIndex + chunkSize, totalItems);
-            List<JobPostingDto> subList = list.subList(startIndex, endIndex);
+            List<JobPostingDto> subList = updatedJobPostings.subList(startIndex, endIndex);
             String attributeName = "list" + (startIndex / chunkSize + 1); // 새로운 속성 이름 생성 (list1, list2, ...)
             model.addAttribute(attributeName, subList);
         }
         
-    	return "index";
+        return "index";
     }
 }
