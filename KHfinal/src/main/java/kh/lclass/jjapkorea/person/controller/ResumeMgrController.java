be13df0bdcb293052521771baaa454fbad354429
Solbeen0531 <netps0201@gmail.com>
2023-10-11 16:55:27 +0900

    Update ResumeMgrController.java
    href="<%=request.getContextPath() %>/person/resume"로 변경

--- KHfinal/src/main/java/kh/lclass/jjapkorea/person/controller/ResumeMgrController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/person/controller/ResumeMgrController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ResumeMgrController {
 
-	@GetMapping("resume")
+	@GetMapping("/person/resume")
 	public String resumeMgr() {
 
 		return "/resume/resumeMgr";
