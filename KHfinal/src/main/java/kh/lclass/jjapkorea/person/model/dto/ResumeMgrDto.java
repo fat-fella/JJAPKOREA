631392ace047fd851793fdb6ae447a81af1b4097
Solbeen0531 <netps0201@gmail.com>
2023-10-12 16:01:46 +0900

    Update ResumeMgrDto.java

--- KHfinal/src/main/java/kh/lclass/jjapkorea/person/model/dto/ResumeMgrDto.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/person/model/dto/ResumeMgrDto.java
@@ -1,5 +1,17 @@
 package kh.lclass.jjapkorea.person.model.dto;
 
+import lombok.Data;
+
+@Data
 public class ResumeMgrDto {
+	private int aid;
+	private String mid;
+	private String pemail;
+	private String pphone;
+	private String phphone;
+	private String pname;
+	private String pgender;
+	private String pbirth;
+	private String padress;
 
 }
