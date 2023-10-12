16aac59c3c7f2b2da8abe5ccaa14e0557d8552f3
yurim0227 <byurim227@gmail.com>
2023-10-13 04:08:23 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/person/controller/ResumeWriteController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/person/controller/ResumeWriteController.java
@@ -2,12 +2,20 @@ package kh.lclass.jjapkorea.person.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ResumeWriteController {
 
 	@GetMapping("/person/resume/write")
 	public String resumeMgr() {
+
+		return "/resume/resumeWrite";
+
+	}
+	
+	@PostMapping("/person/resume/write")
+	public String resumeMgr(String mid) {
 
 		return "/resume/resumeWrite";
 
