bb7eb3253f9979cb1048e439efc56f756b66ef60
Solbeen0531 <netps0201@gmail.com>
2023-10-13 09:20:58 +0900

    Update MailSendDao.java

--- KHfinal/src/main/java/kh/lclass/jjapkorea/mail/model/dao/MailSendDao.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/mail/model/dao/MailSendDao.java
@@ -4,16 +4,14 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import kh.lclass.jjapkorea.mail.model.dto.MailSendDto;
-
-@Repository
-public class MailSendDao {
-	
-	@Autowired
-	private SqlSession sqlSession;
-	
-	public String selectEmail(MailSendDto dto) {
-		return sqlSession.selectOne("mailSend.selectEmail", dto);
-	}
-	
-}
+//@Repository
+//public class MailSendDao {
+//	
+//	@Autowired
+//	private SqlSession sqlSession;
+//	
+//	public String selectEmail(String pemail) throws Exception {
+//		return sqlSession.selectOne("mailSend.selectEmail", pemail);
+//	}
+//	
+//}
