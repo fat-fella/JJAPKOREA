adf7212c1bde0113a76260551d97177d5f121b92
yurim0227 <byurim227@gmail.com>
2023-10-18 15:31:40 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/person/controller/ResumeWriteController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/person/controller/ResumeWriteController.java
@@ -38,118 +38,78 @@ public class ResumeWriteController {
 		model.addAttribute("getResumeWithAward", getResumeWithAward);
 		return "resume/resumeWrite";
 	}
-	
+
 	@PostMapping("/person/resume/write")
-	public String resumeMgr(Model model, ResumeWriteDto resumeWriteDto,
-	                        @RequestParam List<String> qualificationName,
-	                        @RequestParam List<String> qualificationDate,
-	                        @RequestParam List<String> qualificationAuthority,
-	                        @RequestParam List<String> educationPeriod,
-	                        @RequestParam List<String> schoolName,
-	                        @RequestParam List<String> major,
-	                        @RequestParam List<String> workPeriod,
-	                        @RequestParam List<String> workPlace,
-	                        @RequestParam List<String> workDetails,
-	                        @RequestParam List<String> awardName,
-	                        @RequestParam List<String> awardingAuthority,
-	                        @RequestParam List<String> awardDate) throws Exception {
+	public String resumeMgr(Model model, ResumeWriteDto resumeWriteDto, @RequestParam List<String> qualificationName,
+			@RequestParam List<String> qualificationDate, @RequestParam List<String> qualificationAuthority,
+			@RequestParam List<String> educationPeriod, @RequestParam List<String> schoolName,
+			@RequestParam List<String> major, @RequestParam List<String> workPeriod,
+			@RequestParam List<String> workPlace, @RequestParam List<String> workDetails,
+			@RequestParam List<String> awardName, @RequestParam List<String> awardingAuthority,
+			@RequestParam List<String> awardDate) throws Exception {
 		String mid = (String) model.getAttribute("mid");
 
 		// 이력서 정보 추가 또는 업데이트
 		List<ResumeWriteDto> resumeWriteDtos = resumeWriteService.getResumeById(mid);
+		int resumeId;
 
 		if (!resumeWriteDtos.isEmpty()) {
 			// 이미 있는 경우: 업데이트
-			int resumeId = resumeWriteDtos.get(0).getResumeId();
-	        resumeWriteService.updateResume(resumeWriteDto);
-	        
-	        // 자격증 업데이트
-	        resumeWriteService.deleteQualification(resumeId);
-		    for (int i = 1; i < qualificationName.size(); i++) {
-		        QualificationDto qualificationDto = new QualificationDto();
-		        qualificationDto.setResumeId(resumeId);
-		        qualificationDto.setQualificationName(qualificationName.get(i));
-		        qualificationDto.setQualificationDate(qualificationDate.get(i));
-		        qualificationDto.setQualificationAuthority(qualificationAuthority.get(i));
-		        resumeWriteService.insertQualification(qualificationDto);
-		    }
+			resumeId = resumeWriteDtos.get(0).getResumeId();
+			resumeWriteService.updateResume(resumeWriteDto);
 
-		    // 학력 업데이트
-		    resumeWriteService.deleteEducation(resumeId);
-		    for (int i = 1; i < educationPeriod.size(); i++) {
-		        EducationDto educationDto = new EducationDto();
-		        educationDto.setResumeId(resumeId);
-		        educationDto.setEducationPeriod(educationPeriod.get(i));
-		        educationDto.setSchoolName(schoolName.get(i));
-		        educationDto.setMajor(major.get(i));
-		        resumeWriteService.insertEducation(educationDto);
-		    }
+			// 기존 데이터 삭제
+			resumeWriteService.deleteQualification(resumeId);
+			resumeWriteService.deleteEducation(resumeId);
+			resumeWriteService.deleteExperience(resumeId);
+			resumeWriteService.deleteAward(resumeId);
+		} else {
+			// 없는 경우: 추가
+			resumeWriteDto.setMid(mid);
+			resumeWriteService.insertResume(resumeWriteDto);
+			resumeId = resumeWriteDto.getResumeId();
+		}
 
-		    // 경력 업데이트
-		    resumeWriteService.deleteExperience(resumeId);
-		    for (int i = 1; i < workPeriod.size(); i++) {
-		        ExperienceDto experienceDto = new ExperienceDto();
-		        experienceDto.setResumeId(resumeId);
-		        experienceDto.setWorkPeriod(workPeriod.get(i));
-		        experienceDto.setWorkPlace(workPlace.get(i));
-		        experienceDto.setWorkDetails(workDetails.get(i));
-		        resumeWriteService.insertExperience(experienceDto);
-		    }
+		// 나머지 데이터 추가
+		// 자격증 추가
+		for (int i = 1; i < qualificationName.size(); i++) {
+			QualificationDto qualificationDto = new QualificationDto();
+			qualificationDto.setResumeId(resumeId);
+			qualificationDto.setQualificationName(qualificationName.get(i));
+			qualificationDto.setQualificationDate(qualificationDate.get(i));
+			qualificationDto.setQualificationAuthority(qualificationAuthority.get(i));
+			resumeWriteService.insertQualification(qualificationDto);
+		}
+		// 학력 추가
+		for (int i = 1; i < educationPeriod.size(); i++) {
+			EducationDto educationDto = new EducationDto();
+			educationDto.setResumeId(resumeId);
+			educationDto.setEducationPeriod(educationPeriod.get(i));
+			educationDto.setSchoolName(schoolName.get(i));
+			educationDto.setMajor(major.get(i));
+			resumeWriteService.insertEducation(educationDto);
+		}
 
-		    // 수상내역 업데이트
-		    resumeWriteService.deleteAward(resumeId);
-		    for (int i = 1; i < awardName.size(); i++) {
-		        AwardDto awardDto = new AwardDto();
-		        awardDto.setResumeId(resumeId);
-		        awardDto.setAwardName(awardName.get(i));
-		        awardDto.setAwardingAuthority(awardingAuthority.get(i));
-		        awardDto.setAwardDate(awardDate.get(i));
-		        resumeWriteService.insertAward(awardDto);
-		    }
-	    } else {
-	        // 없는 경우: 추가
-	        resumeWriteDto.setMid(mid);
-	        resumeWriteService.insertResume(resumeWriteDto);
-	        // 자격증 추가
-		    for (int i = 1; i < qualificationName.size(); i++) {
-		        QualificationDto qualificationDto = new QualificationDto();
-		        qualificationDto.setResumeId(resumeWriteDto.getResumeId());
-		        qualificationDto.setQualificationName(qualificationName.get(i));
-		        qualificationDto.setQualificationDate(qualificationDate.get(i));
-		        qualificationDto.setQualificationAuthority(qualificationAuthority.get(i));
-		        resumeWriteService.insertQualification(qualificationDto);
-		    }
-		    // 학력 추가
-		    for (int i = 1; i < educationPeriod.size(); i++) {
-		        EducationDto educationDto = new EducationDto();
-		        educationDto.setResumeId(resumeWriteDto.getResumeId());
-		        educationDto.setEducationPeriod(educationPeriod.get(i));
-		        educationDto.setSchoolName(schoolName.get(i));
-		        educationDto.setMajor(major.get(i));
-		        resumeWriteService.insertEducation(educationDto);
-		    }
+		// 경력 추가
+		for (int i = 1; i < workPeriod.size(); i++) {
+			ExperienceDto experienceDto = new ExperienceDto();
+			experienceDto.setResumeId(resumeId);
+			experienceDto.setWorkPeriod(workPeriod.get(i));
+			experienceDto.setWorkPlace(workPlace.get(i));
+			experienceDto.setWorkDetails(workDetails.get(i));
+			resumeWriteService.insertExperience(experienceDto);
+		}
 
-		    // 경력 추가
-		    for (int i = 1; i < workPeriod.size(); i++) {
-		        ExperienceDto experienceDto = new ExperienceDto();
-		        experienceDto.setResumeId(resumeWriteDto.getResumeId());
-		        experienceDto.setWorkPeriod(workPeriod.get(i));
-		        experienceDto.setWorkPlace(workPlace.get(i));
-		        experienceDto.setWorkDetails(workDetails.get(i));
-		        resumeWriteService.insertExperience(experienceDto);
-		    }
+		// 수상내역 추가
+		for (int i = 1; i < awardName.size(); i++) {
+			AwardDto awardDto = new AwardDto();
+			awardDto.setResumeId(resumeId);
+			awardDto.setAwardName(awardName.get(i));
+			awardDto.setAwardingAuthority(awardingAuthority.get(i));
+			awardDto.setAwardDate(awardDate.get(i));
+			resumeWriteService.insertAward(awardDto);
+		}
 
-		    // 수상내역 추가
-		    for (int i = 1; i < awardName.size(); i++) {
-		        AwardDto awardDto = new AwardDto();
-		        awardDto.setResumeId(resumeWriteDto.getResumeId());
-		        awardDto.setAwardName(awardName.get(i));
-		        awardDto.setAwardingAuthority(awardingAuthority.get(i));
-		        awardDto.setAwardDate(awardDate.get(i));
-		        resumeWriteService.insertAward(awardDto);
-		    }
-	    }
-	    
-	    return "redirect:/person/resume/write";
+		return "redirect:/person/resume/write";
 	}
 }
