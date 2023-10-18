08e6a7324f03320331490c0fe5a69cd5e8f09a21
Solbeen0531 <netps0201@gmail.com>
2023-10-18 20:32:21 +0900

    Update MailSendService.jsp - 이메일 보내기 관련
    - 컨트롤러와 내용 분리(모델은 컨트롤러에 있어야 함)
    - 추후 내용 변경 필요
    - 제목만 완료

--- KHfinal/src/main/java/kh/lclass/jjapkorea/mail/model/service/MailSendService.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/mail/model/service/MailSendService.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
+
+import kh.lclass.jjapkorea.business.model.dao.JobPostingDao;
 import kh.lclass.jjapkorea.guest.model.dto.PersonDto;
+import kh.lclass.jjapkorea.mail.model.dto.MailSendDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,8 +21,9 @@ public class MailSendService {
 
 	private final JavaMailSender emailSender;
 
-	public String noticeMail(PersonDto selectOnePerson, Model model) throws MessagingException, IOException {
-
+	
+	public String sendMail(PersonDto selectOnePerson, MailSendDto mailSendDto) throws MessagingException, IOException {
+	
 		// 기타 설정들을 담당할 MimeMesageHelper 객체를 생성
 		// 생성자의 매개값으로 MimeMessage 객체, bool, 문자 인코딩 설정
 		// bool 타입에서 true 매개값을 전달하면 MultiPart(이미지, 오디오도 같이) 형식의 메세지 전달 가능
@@ -30,15 +32,21 @@ public class MailSendService {
 			MimeMessage message = emailSender.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
 
+			
+			// 로그인된 사용자 이메일 받아오기
+			String pemail = selectOnePerson.getPemail(); 
+			
+			
+			System.out.println("===2=======sbpark===="+mailSendDto);
+			
+			
 			helper.setFrom("jjapkorea@naver.com"); // 발신자 주소
-			helper.setSubject("jjapkorea mail title test"); // 메일 제목 설정
+			helper.setSubject("[지원완료]" + mailSendDto.getBizname() + ", 유사한 공고도 확인하세요."); // 메일 제목 설정
 
 			helper.setText("jjapkorea mail content test", true);
 			// 메일 내용 설정
 			// true를 해야 html 형식으로 전송
 
-			selectOnePerson = (PersonDto) model.getAttribute("selectOnePerson");
-			String pemail = selectOnePerson.getPemail();
 
 			// 수신자 전송
 			helper.setTo(pemail);
@@ -48,6 +56,7 @@ public class MailSendService {
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
+		
 		return "index";
 	}
 
