32cef5386ec6868477c793e38902d01791a81831
Solbeen0531 <netps0201@gmail.com>
2023-10-18 20:30:01 +0900

    Update MailSendController.java - 이메일 보내기 완료
    - scrap 컨트롤러를 참고하여 유사하게 재작성되었음
    - scrap dto를 참고하여 mailsend dto가 작성되었음 (해당 내용이 잡 인포 jsp sendMail 함수 ajax 내용에 쓰임)
    - 서비스와 컨트롤러의 역할 분리에 따른 모델 위치 재정의되었음

--- KHfinal/src/main/java/kh/lclass/jjapkorea/mail/controller/MailSendController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/mail/controller/MailSendController.java
@@ -7,7 +7,14 @@ import javax.mail.MessagingException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kh.lclass.jjapkorea.business.model.dao.JobPostingDao;
 import kh.lclass.jjapkorea.guest.model.dto.PersonDto;
+import kh.lclass.jjapkorea.mail.model.dto.MailSendDto;
 import kh.lclass.jjapkorea.mail.model.service.MailSendService;
 
 @Controller
@@ -15,14 +22,22 @@ public class MailSendController {
 
 	private final MailSendService mailService;
 
+	// ajax로 Controller가 실행되면 Service로 이동
 	public MailSendController(MailSendService mailService) {
 		this.mailService = mailService;
 	}
 	
-	@PostMapping("/person/noticeMail")
-	public String noticeMail(PersonDto selectOnePerson, Model model) throws MessagingException, IOException {
-		mailService.noticeMail(selectOnePerson, model);
+	// Service에서 사용되는 값을 받아옴
+	@RequestMapping(value = "/person/noticeMail", method = RequestMethod.POST, produces = "application/text; charset=UTF-8")
+	@ResponseBody
+	public String noticeMail(PersonDto selectOnePerson, @RequestBody MailSendDto mailSendDto, Model model) throws MessagingException, IOException {
+		selectOnePerson = (PersonDto) model.getAttribute("selectOnePerson");
+		// 현재 채용공고 페이지의 회사명 받아오기
+//		getJobPostingsWithBusinessInfo = (JobPostingDao) model.getAttribute("getJobPostingsWithBusinessInfo");
+		mailService.sendMail(selectOnePerson, mailSendDto);
 		return "index";
 	}
+	
+	
 
 }
