ed3c6e2d9abc1f4b42343dff357f5d6f60b61040
Solbeen0531 <netps0201@gmail.com>
2023-10-19 11:08:25 +0900

    Update MailSendController.java
    -메일 내용에 채용공고 제목을 추가하기 위해 속성 추가함

--- KHfinal/src/main/java/kh/lclass/jjapkorea/mail/controller/MailSendController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/mail/controller/MailSendController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import kh.lclass.jjapkorea.business.model.dto.JobPostingDto;
 import kh.lclass.jjapkorea.guest.model.dto.PersonDto;
 import kh.lclass.jjapkorea.mail.model.dto.MailSendDto;
 import kh.lclass.jjapkorea.mail.model.service.MailSendService;
@@ -24,17 +25,16 @@ public class MailSendController {
 	public MailSendController(MailSendService mailService) {
 		this.mailService = mailService;
 	}
-	
+
 	// Service에서 사용되는 값을 받아옴
 	@RequestMapping(value = "/person/noticeMail", method = RequestMethod.POST, produces = "application/text; charset=UTF-8")
 	@ResponseBody
-	public String noticeMail(PersonDto selectOnePerson, @RequestBody MailSendDto mailSendDto, Model model) throws MessagingException, IOException {
+	public String noticeMail(PersonDto selectOnePerson, @RequestBody MailSendDto mailSendDto,
+			JobPostingDto jobPostingDto, Model model) throws MessagingException, IOException {
 		selectOnePerson = (PersonDto) model.getAttribute("selectOnePerson");
 		// 현재 채용공고 페이지의 회사명 받아오기
-		mailService.sendMail(selectOnePerson, mailSendDto);
+		mailService.sendMail(selectOnePerson, mailSendDto, jobPostingDto);
 		return "index";
 	}
-	
-	
 
 }
