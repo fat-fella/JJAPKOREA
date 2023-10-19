54f99fe623731445463e4922809af67279745b94
Solbeen0531 <netps0201@gmail.com>
2023-10-19 11:07:48 +0900

    Update MailSendService.java
    - 메일 내용에 채용공고 제목을 추가하기 위해 속성을 추가함

--- KHfinal/src/main/java/kh/lclass/jjapkorea/mail/model/service/MailSendService.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/mail/model/service/MailSendService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import kh.lclass.jjapkorea.business.model.dto.JobPostingDto;
 import kh.lclass.jjapkorea.guest.model.dto.PersonDto;
 import kh.lclass.jjapkorea.mail.model.dto.MailSendDto;
 import lombok.RequiredArgsConstructor;
@@ -20,8 +21,7 @@ public class MailSendService {
 
 	private final JavaMailSender emailSender;
 
-	
-	public String sendMail(PersonDto selectOnePerson, MailSendDto mailSendDto) throws MessagingException, IOException {
+	public String sendMail(PersonDto selectOnePerson, MailSendDto mailSendDto, JobPostingDto jobPostingDto) throws MessagingException, IOException {
 	
 		// 기타 설정들을 담당할 MimeMesageHelper 객체를 생성
 		// 생성자의 매개값으로 MimeMessage 객체, bool, 문자 인코딩 설정
@@ -39,7 +39,52 @@ public class MailSendService {
 			helper.setFrom("jjapkorea@naver.com"); // 발신자 주소
 			helper.setSubject("[지원완료] " + mailSendDto.getBizname() + ", 유사한 공고도 확인하세요."); // 메일 제목 설정
 
-			helper.setText("jjapkorea mail content test", true);
+			
+			StringBuffer sbuffer = new StringBuffer();
+			
+			sbuffer.append("<style>"
+					+ ".mailSend {"
+					+ "bgcolor: 'ededed';"
+					+ "width: 100%;"
+					+ "height: 100%;"
+					+ "}"
+					+ " "
+					+ "td {font-size: 9pt;}"
+					+ "</style>");
+			
+			sbuffer.append("<div class='mailSend'>");
+			sbuffer.append("<div>");
+			sbuffer.append("<img src='resources/images/JJAPkorea_logo(불투명흰색).png'/>");
+			
+			sbuffer.append("<div>");
+			sbuffer.append(mailSendDto.getBizname()+" 채용공고에 <br> 입사지원이 완료되었습니다.");
+			sbuffer.append("</div>");
+			
+			sbuffer.append("<table cellpadding=5 cellspacing=0 border=1 style='border-collapse:collapse' boardcolor='888888'>");
+			sbuffer.append(" <tr>");
+			sbuffer.append(" <td align=center bgcolor='ECECEC' width=140>"
+					+ mailSendDto.getBizname()
+					+ "</td>");
+			sbuffer.append(" </tr>");
+			
+			sbuffer.append(" <tr>");
+			sbuffer.append(" <td width=170>"
+					+ mailSendDto.getRetitle()
+					+ "</td>");
+			sbuffer.append(" </tr>");
+			sbuffer.append("</table>");
+			sbuffer.append("</div>");
+			
+			sbuffer.append("<div id='jpostInfo'>");
+			sbuffer.append("<form action='http://localhost:8090/jjapkorea/jobpostinginfo?jid=mailSendDto.getJid()' method='post'>");
+			sbuffer.append("<br><br>");
+			sbuffer.append("<button type='button'>지원자 현황 보기</button>");
+			sbuffer.append("</form>");
+			sbuffer.append("</div>");
+			
+			
+//			helper.setText("jjapkorea mail content test", true); 메일 전송 확인 후 주석 처리 함
+			helper.setText(sbuffer.toString(), true);
 			// 메일 내용 설정
 			// true를 해야 html 형식으로 전송
 
