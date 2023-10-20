2a6cd98dc0e511c20f881d612bb05b37c9846767
Solbeen0531 <netps0201@gmail.com>
2023-10-20 14:56:06 +0900

    Update JobPostingInfoController.java
    Remove unused imports

--- KHfinal/src/main/java/kh/lclass/jjapkorea/business/controller/JobPostingInfoController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/business/controller/JobPostingInfoController.java
@@ -1,15 +1,12 @@
 package kh.lclass.jjapkorea.business.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
