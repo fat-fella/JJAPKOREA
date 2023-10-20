f06c3fa2a3569d61f9b4d4833c0ec1d374e596fe
Solbeen0531 <netps0201@gmail.com>
2023-10-20 15:01:08 +0900

    Update APISnsPsignupController.java
    Remove unused imports

--- KHfinal/src/main/java/kh/lclass/jjapkorea/swp/controller/APISnsPsignupController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/swp/controller/APISnsPsignupController.java
@@ -19,7 +19,6 @@ import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
