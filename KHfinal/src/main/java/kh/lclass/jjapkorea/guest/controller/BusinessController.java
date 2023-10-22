bf08023634420db325f7f2334339940e70639949
yurim0227 <byurim227@gmail.com>
2023-10-23 01:43:38 +0900

    .

--- KHfinal/src/main/java/kh/lclass/jjapkorea/guest/controller/BusinessController.java
+++ KHfinal/src/main/java/kh/lclass/jjapkorea/guest/controller/BusinessController.java
@@ -31,7 +31,9 @@ public class BusinessController {
 	}
 	
 	@RequestMapping(value = "/businessInfo", method = RequestMethod.POST)
-	public String BusinessInfo(String mid) throws Exception {
+	public String BusinessInfo(String mid, Model model) throws Exception {
+		List<Map<String, Object>> getBusinessWithBusinessformByMid = memberServiceImpl.getBusinessWithBusinessformByMid(mid);
+		model.addAttribute("getBusinessWithBusinessformByMid", getBusinessWithBusinessformByMid);
 		return "businessInfo";
 	}
 }
